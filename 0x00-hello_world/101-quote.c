c7f479d0a20f9ed76785d7f73dea1f9da0add860
sim <sim@example.com>
2023-02-16 18:40:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - a c program that prints a line to the standard error
+ * Return: 1
+ */
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
