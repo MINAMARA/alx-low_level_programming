03a1aac3e147c22cbfa6513740f27c2ea17ba010
sim <sim@example.com>
2023-02-17 19:06:25 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int c = 0;
-	 while (c < 10)
-	 {
-		 putchar(48 + c);
-		 if (c != 9)
-		 {
-			 putchar(',');
-			 putchar(' ');
-		 }
-		 c++;
-	 }
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
 }
