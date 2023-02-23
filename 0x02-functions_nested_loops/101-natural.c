6d39c9c58221748488b7990de2ba4770774859f0
sim <sim@example.com>
2023-02-22 17:07:40 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - 101-natural.c
+ *
+ * Description - 101-natural.c
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+		printf("%d\n", sum);
+
+		return (0);
+	}
+}
