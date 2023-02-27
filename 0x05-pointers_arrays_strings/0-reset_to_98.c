44b2e42ca1180d8f96ddec692f0c401b552126a7
sim <sim@example.com>
2023-02-27 15:59:20 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *rest_to-98 -function that takes a pointer to an int as parameter and
- *updates the value it ponts to 98
+ * reset_to_98 -function that takes a pointer to an int as parameter and
+ * updates the value it ponts to 98
  *
  * @n: ths is thr entry
  *
