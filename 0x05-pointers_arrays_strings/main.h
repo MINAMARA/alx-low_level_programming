e3d1a0661fe0b9c6189c4a62847623c029a4b8c6
sim <sim@example.com>
2023-02-28 15:11:04 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-
+void _putchar (char c); 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
