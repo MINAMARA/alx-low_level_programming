1f665d832a4c8c4efa93e6a9d15b847ef020f43a
MINAMARA <simeonmaranatha@gmail.com>
2023-04-10 13:07:40 -0700

    include "main.h"
    /**
     * get_bit - Fuction that gets the value of a bit at a given index.
     * @n: The bit int.
     * @index: index gets the value at 0.
     *
     * Return: If an error occurs - -1 or value of the bit at index
     */
    int get_bit(unsigned long int n, unsigned int index)
    {
    if (index >= sizeof(n) * 8)
    return (-1);
    
    return ((n >> index) & 1);
    }

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * get_bit - Fuction that gets the value of a bit at a given index.
+ * @n: The bit int.
+ * @index: index gets the value at 0.
+ *
+ * Return: If an error occurs - -1 or value of the bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
